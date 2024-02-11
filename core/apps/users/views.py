e5530f59266a81b1c58ed503be8ca06b8252b3bf
elly-hacen <elly.hacen@gmail.com>
2024-02-11 21:00:38 +0500

    Remove unused imports

--- core/apps/users/views.py
+++ core/apps/users/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 
 from django.views import View
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
 from django.views.generic import CreateView, TemplateView, DeleteView, ListView, UpdateView
 
 from django.contrib.messages.views import SuccessMessageMixin
