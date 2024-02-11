9760530e19e3b373bc3c80927798495dbff32d48
elly-hacen <elly.hacen@gmail.com>
2024-02-11 20:56:47 +0500

    Apply generic class-based views for enhanced code

--- core/apps/users/views.py
+++ core/apps/users/views.py
@@ -2,17 +2,15 @@ from django.contrib import messages
 from django.contrib.auth import get_user_model, logout
 from django.http import HttpResponseRedirect, HttpResponse
 
-from django.shortcuts import render, redirect, get_object_or_404
-from django.utils.decorators import method_decorator
 from django.views import View
-from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import CreateView, TemplateView, DeleteView, ListView, UpdateView
 
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.utils.http import urlsafe_base64_decode
 
@@ -24,39 +22,51 @@ from core.apps.users.forms import UserSignupForm, UserLoginForm, UserEditForm, B
 User = get_user_model()
 
 
-class EditUserDetailsView(LoginRequiredMixin, View):
+class EditUserDetailsView(LoginRequiredMixin, UpdateView):
+    """ View for editing user details. """
+
+    form_class = UserEditForm
     template_name = 'accounts/user/edit_details.html'
+    success_url = reverse_lazy("users:edit_details")
 
-    def post(self, request, *args, **kwargs):
-        user_form = UserEditForm(instance=request.user, data=request.POST)
-        if user_form.is_valid():
-            user_form.save()
-        return render(request, self.template_name, {'user_form': user_form})
+    def get_object(self, queryset=None):
+        return self.request.user
 
-    def get(self, request, *args, **kwargs):
-        user_form = UserEditForm(instance=request.user)
-        return render(request, self.template_name, {'user_form': user_form})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_form'] = UserEditForm(instance=self.request.user)
+        return context
 
 
 class UserDeleteConfirmView(TemplateView, LoginRequiredMixin):
     template_name = 'accounts/user/delete_confirm.html'
 
 
-class UserDeactivateView(TemplateView):
-    template_name = 'accounts/user/delete_user.html'
+class UserDeactivateView(DeleteView):
+    """  View for deactivating a user account. """
 
-    def post(self, request):
-        user = User.objects.get(username=request.user)
+    template_name = 'accounts/user/delete_user.html'
+    success_url = reverse_lazy('users:delete_user_confirm')
+
+    def get_object(self, queryset=None):
+        """Returns the current user as the object to be deactivated."""
+        return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        user = self.get_object()
         user.is_active = False
         user.save()
         logout(request)
-        return redirect('users:delete_user_confirm')
+        return super().delete(request, *args, **kwargs)
 
 
-class AccountSettingsView(View):
-    @method_decorator(login_required)
-    def get(self, request):
-        return render(request, 'accounts/user/account-settings.html')
+class AccountSettingsView(TemplateView, LoginRequiredMixin):
+    template_name = 'accounts/user/account-settings.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('users:login')
+        return super(AccountSettingsView, self).dispatch(request, *args, **kwargs)
 
 
 class UserAccountEmailConfirmView(TemplateView):
@@ -64,6 +74,8 @@ class UserAccountEmailConfirmView(TemplateView):
 
 
 class UserAccountActivationView(TemplateView):
+    """  View for user account activation. """
+
     def get(self, request, *args, **kwargs):
         try:
             user_id = urlsafe_base64_decode(kwargs['uid']).decode()
@@ -101,12 +113,13 @@ class UserSignupView(CreateView, OnlyUnauthenticatedMixin):
 
 
 class UserLoginView(LoginView):
-    template_name = 'accounts/registration/login.html'
-    success_url = reverse_lazy('library:books_home')
+    """ Custom user login view. """
+
     form_class = UserLoginForm
+    success_url = reverse_lazy('library:books_home')
+    template_name = 'accounts/registration/login.html'
 
     def form_valid(self, form):
-        print(form.cleaned_data)
         email = form.cleaned_data['username']  # This is an email field
         password = form.cleaned_data['password']
         user = authenticate(self.request, username=email, password=password)
@@ -118,8 +131,9 @@ class UserLoginView(LoginView):
             return self.form_invalid(form)
 
 
-class UserLogoutView(LoginRequiredMixin, LogoutView, SuccessMessageMixin):
+class UserLogoutView(LoginRequiredMixin, SuccessMessageMixin, LogoutView):
     success_message = 'You have been logged out.'
+    success_url = reverse_lazy('Library:books_home')
 
     def get_success_url(self):
         return reverse_lazy('library:books_home')
@@ -136,24 +150,31 @@ class BookUploadView(CreateView):
         return kwargs
 
 
-class FavoriteListView(View, LoginRequiredMixin):
+class FavoriteListView(LoginRequiredMixin, ListView):
+    model = Book
+    context_object_name = 'favorite'
+    template_name = 'accounts/user/add_to_favorite.html'
 
-    def get(self, request, *args, **kwargs):
-        user = request.user
-        books = Book.objects.filter(favorites=user)
-        return render(request, 'accounts/user/add_to_favorite.html', {
-            'favorite': books,
-        })
+    def get_queryset(self):
+        user = self.request.user
+        return Book.objects.filter(favorites=user)
 
 
 class AddToFavoriteView(View, LoginRequiredMixin):
-    def get(self, request, bid, *args, **kwargs):  # bid => book id
-        book = get_object_or_404(Book, id=bid)
-        if book.favorites.filter(id=request.user.id).exists():
-            book.favorites.remove(request.user)
-            messages.success(request, f"{book.title} is removed from your favorites.")
-        else:
-            book.favorites.add(request.user)
-            messages.success(request, f"{book.title} is added to your favorites.")
+    """ View for adding or removing a book from the user's favorites. """
 
+    def get(self, request, book_id, *args, **kwargs):
+
+        book = get_object_or_404(Book, id=book_id)
+        user = request.user
+        
+        if book.favorites.filter(id=user.id).exists():
+            book.favorites.remove(user)
+            action = 'removed from'
+
+        else:
+            book.favorites.add(user)
+            action = 'added to'
+
+        messages.success(request, f"{book.title} is {action} your favorites.")
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
